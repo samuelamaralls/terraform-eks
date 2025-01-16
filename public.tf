3913ab8bee6fa7a6c2fc0cbe506222df3692b698
samuelamaralls <rodrigues.samuel1221@gmail.com>
2025-01-16 15:42:48 -0300

    feat: tags, cidrsubnet function

---
+++ public.tf
@@ -0,0 +1,44 @@
+resource "aws_subnet" "example_eks_public_1a" {
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 4, 0)
+  availability_zone       = "${data.aws_region.current.name}a"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "Example - subnet -1a",
+      "kubernetes.io/role/elb" = 1
+    }
+  )
+}
+
+resource "aws_subnet" "example_eks_public_1b" {
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 4, 1)
+  availability_zone       = "${data.aws_region.current.name}b"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "Example - subnet -1b",
+      "kubernetes.io/role/elb" = 1
+    }
+  )
+}
+
+resource "aws_subnet" "example_eks_public_1c" {
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 4, 2)
+  availability_zone       = "${data.aws_region.current.name}c"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "Example - subnet -1c",
+      "kubernetes.io/role/elb" = 1
+    }
+  )
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "cidr_block" {
+  type        = string
+  description = "Networkind CIDR block VPC"
+}
