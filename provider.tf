96511ea2956ed580ec2f0c3f583977ad30338df6
samuelamaralls <rodrigues.samuel1221@gmail.com>
2025-01-16 14:28:30 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.83.1"
     }
+  }
+  backend "s3" {
+    bucket = "example-eks"
+    key    = "hml/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
